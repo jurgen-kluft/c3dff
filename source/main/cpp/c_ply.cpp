62689261471a9f92b1daa4a07640858f954f43b4
Jurgen <jurgen.kluft@gmail.com>
2022-09-06 19:48:21 +0800

    * PlyType; unsigned and type_sizeof/type_islist/type_is_int/...

--- source/main/cpp/c_ply.cpp
+++ source/main/cpp/c_ply.cpp
@@ -8,20 +8,27 @@ namespace ncore
     {
         enum PlyType
         {
-            TYPE_SIGNED  = 0x100,
-            TYPE_FLOAT   = 0x200,
-            TYPE_LIST    = 0x400,
-            TYPE_INVALID = 0,
-            TYPE_INT8    = 0x1 | TYPE_SIGNED,
-            TYPE_UINT8   = 0x1,
-            TYPE_INT16   = 0x2 | TYPE_SIGNED,
-            TYPE_UINT16  = 0x2,
-            TYPE_INT32   = 0x4 | TYPE_SIGNED,
-            TYPE_UINT32  = 0x4,
-            TYPE_FLOAT32 = 0x4 | TYPE_FLOAT,
-            TYPE_FLOAT64 = 0x8 | TYPE_FLOAT,
-            MASK_SIZE    = 0xFF,
+            TYPE_SIGNED    = 0x100,
+            TYPE_UNSIGNED  = 0x200,
+            TYPE_FLOAT     = 0x400,
+            TYPE_LIST      = 0x800,
+            TYPE_INVALID   = 0,
+            TYPE_INT8      = 0x1 | TYPE_SIGNED,
+            TYPE_UINT8     = 0x1 | TYPE_UNSIGNED,
+            TYPE_INT16     = 0x2 | TYPE_SIGNED,
+            TYPE_UINT16    = 0x2 | TYPE_UNSIGNED,
+            TYPE_INT32     = 0x4 | TYPE_SIGNED,
+            TYPE_UINT32    = 0x4 | TYPE_UNSIGNED,
+            TYPE_FLOAT32   = 0x4 | TYPE_FLOAT,
+            TYPE_FLOAT64   = 0x8 | TYPE_FLOAT,
+            TYPE_SIZE_MASK = 0xFF,
         };
+
+        inline static s32  type_sizeof(PlyType type) { return (type & TYPE_SIZE_MASK); }
+        inline static bool type_islist(PlyType type) { return (type & TYPE_LIST) == TYPE_LIST; }
+        inline static bool type_is_int(PlyType type) { return (type & (TYPE_UNSIGNED | TYPE_SIGNED)) == (TYPE_UNSIGNED | TYPE_SIGNED); }
+        inline static bool type_is_f32(PlyType type) { return (type & TYPE_FLOAT32) == TYPE_FLOAT32; }
+        inline static bool type_is_f64(PlyType type) { return (type & TYPE_FLOAT64) == TYPE_FLOAT64; }
 
         struct PlyString
         {
@@ -86,11 +93,13 @@ namespace ncore
 
         static bool operator==(const PlyString& _this, const char* other) { return PlyString::compare(_this, other) == 0; }
         static bool operator!=(const PlyString& _this, const char* other) { return PlyString::compare(_this, other) != 0; }
+
         static bool operator==(const PlyString& _this, const PlyString& other) { return PlyString::compare(_this, other) == 0; }
         static bool operator!=(const PlyString& _this, const PlyString& other) { return PlyString::compare(_this, other) != 0; }
 
         static PlyType parse_type(PlyString const& str)
         {
+            // @note: Can be optimized a bit by branching on the first character, e.g. 'i', 'u', 'f'
             if (str == "int8" || str == "char")
                 return TYPE_INT8;
             else if (str == "uint8" || str == "uchar")
@@ -110,30 +119,30 @@ namespace ncore
             return TYPE_INVALID;
         }
 
-        template <typename T, typename T2> inline T2 endian_swap(const T& v) noexcept { return v; }
-        template <> inline u16                       endian_swap<u16, u16>(const u16& v) noexcept { return (v << 8) | (v >> 8); }
-        template <> inline u32                       endian_swap<u32, u32>(const u32& v) noexcept { return (v << 24) | ((v << 8) & 0x00ff0000) | ((v >> 8) & 0x0000ff00) | (v >> 24); }
-        template <> inline u64                       endian_swap<u64, u64>(const u64& v) noexcept
+        template <typename T, typename T2> inline T2 endian_swap(const T& v) { return v; }
+        template <> inline u16                       endian_swap<u16, u16>(const u16& v) { return (v << 8) | (v >> 8); }
+        template <> inline u32                       endian_swap<u32, u32>(const u32& v) { return (v << 24) | ((v << 8) & 0x00ff0000) | ((v >> 8) & 0x0000ff00) | (v >> 24); }
+        template <> inline u64                       endian_swap<u64, u64>(const u64& v)
         {
             return (((v & 0x00000000000000ffLL) << 56) | ((v & 0x000000000000ff00LL) << 40) | ((v & 0x0000000000ff0000LL) << 24) | ((v & 0x00000000ff000000LL) << 8) | ((v & 0x000000ff00000000LL) >> 8) | ((v & 0x0000ff0000000000LL) >> 24) |
                     ((v & 0x00ff000000000000LL) >> 40) | ((v & 0xff00000000000000LL) >> 56));
         }
-        template <> inline s16 endian_swap<s16, s16>(const s16& v) noexcept
+        template <> inline s16 endian_swap<s16, s16>(const s16& v)
         {
             u16 r = endian_swap<u16, u16>(*(u16*)&v);
             return *(s16*)&r;
         }
-        template <> inline s32 endian_swap<s32, s32>(const s32& v) noexcept
+        template <> inline s32 endian_swap<s32, s32>(const s32& v)
         {
             u32 r = endian_swap<u32, u32>(*(u32*)&v);
             return *(s32*)&r;
         }
-        template <> inline s64 endian_swap<s64, s64>(const s64& v) noexcept
+        template <> inline s64 endian_swap<s64, s64>(const s64& v)
         {
             u64 r = endian_swap<u64, u64>(*(u64*)&v);
             return *(s64*)&r;
         }
-        template <> inline f32 endian_swap<u32, f32>(const u32& v) noexcept
+        template <> inline f32 endian_swap<u32, f32>(const u32& v)
         {
             union
             {
@@ -143,7 +152,7 @@ namespace ncore
             i = endian_swap<u32, u32>(v);
             return f;
         }
-        template <> inline f64 endian_swap<u64, f64>(const u64& v) noexcept
+        template <> inline f64 endian_swap<u64, f64>(const u64& v)
         {
             union
             {
@@ -152,20 +161,6 @@ namespace ncore
             };
             i = endian_swap<u64, u64>(v);
             return d;
-        }
-
-        inline u32 hash_fnv1a(const char* str) noexcept
-        {
-            static const u32 fnv1aBase32  = 0x811C9DC5u;
-            static const u32 fnv1aPrime32 = 0x01000193u;
-            u32              result       = fnv1aBase32;
-            while (*str != 0)
-            {
-                char const c = *str++;
-                result ^= static_cast<u32>(c);
-                result *= fnv1aPrime32;
-            }
-            return result;
         }
 
         class PlyAllocator
@@ -304,28 +299,44 @@ namespace ncore
         {
             ctxt->m_hdr = ctxt->m_alloc->construct<PlyHeader>();
 
-            PlyString   line    = read_line(hdr_begin, hdr_end);
-            PlyElement* element = nullptr;
-            bool        success = true;
+            PlyString   line       = read_line(hdr_begin, hdr_end);
+            PlyElement* element    = nullptr;
+            s32         prop_index = 0;
+            bool        success    = true;
             while (line.m_str != line.m_end)
             {
                 PlyString token = read_token(line);
-                if (token == "ply" || token == "PLY" || token.is_empty())
-                    continue;
-                else if (token == "comment")
+                if (token == "comment")
+                {
                     read_header_comment(ctxt, line);
+                }
                 else if (token == "format")
+                {
                     read_header_format(ctxt, line);
+                }
                 else if (token == "element")
-                    element = read_header_element(ctxt, line);
+                {
+                    element    = read_header_element(ctxt, line);
+                    prop_index = 0;
+                }
                 else if (token == "property")
+                {
                     read_header_property(ctxt, element, line);
+                }
                 else if (token == "obj_info")
+                {
                     read_header_obj_info(ctxt, line);
-                else if (token == "end_header")
-                    break;
+                }
                 else
-                    success = false; // unexpected header field
+                {
+                    if (token == "ply" || token == "PLY" || token.is_empty())
+                        continue;
+
+                    if (token == "end_header")
+                        break;
+
+                    success = false;
+                }
 
                 line = read_line(hdr_begin, hdr_end);
             }
@@ -342,13 +353,12 @@ namespace ncore
         void read_header_format(PlyCtxt* ctxt, PlyString& line)
         {
             PlyString token = read_token(line);
-            if (token == "binary_little_endian")
+            if (token == "ascii")
+                ctxt->m_hdr->m_format = FORMAT_ASCII;
+            else if (token == "binary_little_endian")
                 ctxt->m_hdr->m_format = FORMAT_BLE;
             else if (token == "binary_big_endian")
                 ctxt->m_hdr->m_format = FORMAT_BBE;
-            else if (token == "ascii")
-                ctxt->m_hdr->m_format = FORMAT_ASCII;
-
             ctxt->m_hdr->m_version = read_token(line);
         }
 
@@ -358,19 +368,22 @@ namespace ncore
             elem->m_name        = read_token(line);
             PlyString str_count = read_token(line);
             elem->m_prop_count  = parse_u32(str_count);
-            elem->m_prop_array  = (PlyProperty*)ctxt->m_alloc->alloc(sizeof(PlyProperty) * elem->m_prop_count);
+            // Allocate the array of properties
+            elem->m_prop_array = (PlyProperty*)ctxt->m_alloc->alloc(sizeof(PlyProperty) * elem->m_prop_count);
             return elem;
         }
 
-        void read_header_property(PlyCtxt* ctxt, PlyElement* elem, PlyString& line)
+        void read_header_property(PlyCtxt* ctxt, PlyElement* elem, s32 prop_index, PlyString& line)
         {
-            PlyProperty* prop       = ctxt->m_alloc->construct<PlyProperty>();
+            PlyProperty* prop       = &elem->m_prop_array[prop_index];
             PlyString    type_token = read_token(line);
             if (type_token == "list")
             {
                 PlyString count_type_token = read_token(line);
                 PlyString list_type_token  = read_token(line);
                 PlyString property_name    = read_token(line);
+                prop->m_list_count_type    = parse_type(count_type_token);
+                prop->m_property_type      = parse_type(list_type_token) | TYPE_LIST;
                 prop->m_name               = property_name;
             }
             else
