fb09beae6896f41b0f173b44a6ae4fd678583ada
Jurgen <jurgen.kluft@gmail.com>
2022-09-11 16:07:09 +0800

    + go; added folder 'svo_builder'

---
+++ go/svo_builder/main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+
+# Out-Of-Core SVO Builder:
+
+	- Needs a .tri file as input
+	- Partitioning
+	- Voxelizing
+	- SVO Building
+
+There is glm for go: https://github.com/bloeys/gglm
+
+*/
